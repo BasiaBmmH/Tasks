96ca8c7999a1ed4cbab40a6151061bb6679429e1
BasiaBmmH <barbaniella@gmail.com>
2025-05-05 19:30:52 +0200

    15.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,11 +13,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+//    @Autowired
+//    public TaskController(DbService service, TaskMapper taskMapper) {
+//        this.service = service;
+//        this.taskMapper = taskMapper;
+//    }
 
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = (List<Task>) service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping(value = "{taskID}")
@@ -46,6 +62,8 @@ public class TaskController {
     public List<TaskDto> searchTasks() {
         return getTasks();
     }
+
+
 
 
 }
