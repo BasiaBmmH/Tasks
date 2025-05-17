87542b8a729ebf51240c4106868ece95c133d319
BasiaBmmH <barbaniella@gmail.com>
2025-05-17 15:56:38 +0200

    18,4

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,8 +1,12 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Data
+@AllArgsConstructor
 public class TrelloCardDto {
 
     private String name;
